6fc40298b264250b1b7735fea6873e067a6358e3
misaki kawaguchi <60394359+misaki-kawaguchi@users.noreply.github.com>
2024-04-29 19:02:19 +0900

    Deleting a Movie (#4)
    * MovieListViewを作成
    
    * 一覧からmovieを削除できるようにする

---
+++ Movies/Views/MovieListView.swift
@@ -0,0 +1,44 @@
+//
+//  MovieListView.swift
+//  Movies
+//
+//  Created by 川口美咲 on 2024/04/29.
+//
+
+import SwiftUI
+import SwiftData
+
+struct MovieListView: View {
+    let movies: [Movie]
+    @Environment(\.modelContext) private var context
+    
+    private func deleteMovie(IndexSet: IndexSet) {
+        IndexSet.forEach { index in
+            let movie = movies[index]
+            context.delete(movie)
+            do {
+                try context.save()
+            } catch {
+                print(error.localizedDescription)
+            }
+        }
+    }
+    
+    var body: some View {
+        List {
+            ForEach(movies) { movie in
+                HStack {
+                    Text(movie.title)
+                    Spacer()
+                    Text(movie.year.description)
+                }
+            }
+            .onDelete(perform: deleteMovie)
+        }
+    }
+}
+
+#Preview {
+    MovieListView(movies: [])
+        .modelContainer(for: [Movie.self])
+}
