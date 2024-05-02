88944829b0e715b900241fb3a7eb483f574900ca
misaki-kawaguchi <m.kawaguchi1122@gmail.com>
2024-05-02 09:10:48 +0900

    Movieモデルにactorsを追加

--- Movies/Models/Actor.swift
+++ Movies/Models/Actor.swift
@@ -8,8 +8,10 @@
 import Foundation
 import SwiftData
 
+@Model
 final class Actor {
     var name: String
+    var movies: [Movie] = []
     
     init(name: String) {
         self.name = name
--- Movies/Models/Movie.swift
+++ Movies/Models/Movie.swift
@@ -16,6 +16,9 @@ final class Movie {
     @Relationship(deleteRule: .cascade, inverse: \Review.movie) 
     var reviews: [Review] = []
     
+    @Relationship(deleteRule: .noAction, inverse: \Actor.movies)
+    var actors: [Actor] = []
+    
     init(title: String, year: Int) {
         self.title = title
         self.year = year
