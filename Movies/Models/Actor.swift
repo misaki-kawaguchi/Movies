fac8d2bbe1b54cd238f4dabb7fcc3d37f552572b
misaki-kawaguchi <m.kawaguchi1122@gmail.com>
2024-05-02 09:06:01 +0900

    Actorモデルを作成

---
+++ Movies/Models/Actor.swift
@@ -0,0 +1,17 @@
+//
+//  Actor.swift
+//  Movies
+//
+//  Created by 川口美咲 on 2024/05/02.
+//
+
+import Foundation
+import SwiftData
+
+final class Actor {
+    var name: String
+    
+    init(name: String) {
+        self.name = name
+    }
+}
