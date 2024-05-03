a214ce9ba5ab7ec86b538f500a2b3f6aec64253f
misaki-kawaguchi <m.kawaguchi1122@gmail.com>
2024-05-03 12:40:59 +0900

    ActorDetailScreenを作成

---
+++ Movies/Screens/ActorDetailScreen.swift
@@ -0,0 +1,28 @@
+//
+//  ActorDetailScreen.swift
+//  Movies
+//
+//  Created by 川口美咲 on 2024/05/03.
+//
+
+import SwiftUI
+
+struct ActorDetailScreen: View {
+    let actor: Actor
+    @State private var selectedMovies: Set<Movie> = []
+    
+    var body: some View {
+        VStack {
+            MovieSelectionView(selectedMovies: $selectedMovies)
+                .onAppear {
+                    selectedMovies = Set(actor.movies)
+                }
+            
+        }
+        .navigationTitle(actor.name)
+    }
+}
+
+//#Preview {
+//    ActorDetailScreen()
+//}
