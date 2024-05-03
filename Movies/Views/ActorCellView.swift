a309fd7078aa28627dd75f3751c872d8b03735fa
misaki kawaguchi <60394359+misaki-kawaguchi@users.noreply.github.com>
2024-05-03 12:13:25 +0900

    movie詳細画面にactorを表示できるようにする (#15)

---
+++ Movies/Views/ActorCellView.swift
@@ -0,0 +1,24 @@
+//
+//  ActorCellView.swift
+//  Movies
+//
+//  Created by 川口美咲 on 2024/05/03.
+//
+
+import SwiftUI
+
+struct ActorCellView: View {
+    let actor: Actor
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(actor.name)
+            Text(actor.movies.map { $0.title }, format: .list(type: .and))
+                .font(.caption)
+        }
+    }
+}
+
+//#Preview {
+//    ActorCellView()
+//}
