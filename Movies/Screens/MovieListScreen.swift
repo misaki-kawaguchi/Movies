c385241acf1edf06c06cbe8657007538ae583d3b
misaki kawaguchi <60394359+misaki-kawaguchi@users.noreply.github.com>
2024-05-10 19:42:07 +0900

    Movieを検索できるようにする (#19)

--- Movies/Screens/MovieListScreen.swift
+++ Movies/Screens/MovieListScreen.swift
@@ -44,7 +44,7 @@ struct MovieListScreen: View {
                     activeSheet = .showFilter
                 }
             }
-            MovieListView(movies: movies)
+            MovieListView(filterOption: filterOption)
             
             Text("Actors")
                 .font(.largeTitle)
