21d13114f8fc553a276f9212269f51102951d4d9
misaki-kawaguchi <m.kawaguchi1122@gmail.com>
2024-05-13 11:00:19 +0900

    フィルタリングできない不具合を修正

--- Movies/Models/Movie.swift
+++ Movies/Models/Movie.swift
@@ -12,7 +12,11 @@ import SwiftData
 final class Movie {
     var title: String
     var year: Int
-    var genre: Genre
+    var genreId: Int
+    
+    var genre: Genre {
+        Genre(rawValue: genreId)!
+    }
     
     var reviewsCount: Int {
         reviews.count
@@ -30,6 +34,6 @@ final class Movie {
     init(title: String, year: Int, genre: Genre) {
         self.title = title
         self.year = year
-        self.genre = genre
+        self.genreId = genre.id
     }
 }
