26ba99bcf002f5e1a4d50fc8e60074d87a51c3bf
misaki kawaguchi <60394359+misaki-kawaguchi@users.noreply.github.com>
2024-05-13 11:01:43 +0900

    フィルタリングできない不具合を修正 (#29)

--- Movies/Views/MovieListView.swift
+++ Movies/Views/MovieListView.swift
@@ -23,7 +23,7 @@ struct MovieListView: View {
         case .actorsCount(let numberOfActors):
             _movies = Query(filter: #Predicate {$0.actors.count >= numberOfActors })
         case .genre(let genre):
-            _movies = Query(filter: #Predicate { $0.genre == genre })
+            _movies = Query(filter: #Predicate { $0.genreId == genre.id })
         case .none:
             _movies = Query()
         }
