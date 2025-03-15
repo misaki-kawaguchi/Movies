02270f0cc3cdc9caf47055edbd5b33a71a94ce97
misaki-kawaguchi <m.kawaguchi1122@gmail.com>
2025-03-15 17:26:06 +0900

    first commit

---
+++ Movies/MoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesApp.swift
+//  Movies
+//
+//  Created by 川口美咲 on 2025/03/15.
+//
+
+import SwiftUI
+
+@main
+struct MoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
