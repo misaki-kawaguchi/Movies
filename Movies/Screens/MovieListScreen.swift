d1a7646287f680b203b2dfb505ec04d08f68aac0
misaki kawaguchi <60394359+misaki-kawaguchi@users.noreply.github.com>
2025-03-17 10:17:49 +0900

    Fetching and Displaying Movies  (#3)
    * Movie一覧を表示、一覧からAddMovieScreenを開ける様にする
    
    * title追加、saveした後に画面が閉じる様にする
    
    * アルファベット順に並べる

---
+++ Movies/Screens/MovieListScreen.swift
@@ -0,0 +1,41 @@
+//
+//  MovieListScreen.swift
+//  Movies
+//
+//  Created by 川口美咲 on 2025/03/17.
+//
+
+import SwiftUI
+import SwiftData
+
+struct MovieListScreen: View {
+    
+    @Query(sort: \Movie.title, order: .forward) private var movies: [Movie]
+    @State private var isAddMoviePresented: Bool = false
+    
+    var body: some View {
+        List(movies) { movie in
+            Text(movie.title)
+        }
+        .toolbar(content: {
+            ToolbarItem(placement: .topBarTrailing) {
+                Button("Add Movie") {
+                    isAddMoviePresented = true
+                }
+            }
+        })
+        .sheet(isPresented: $isAddMoviePresented, content: {
+            NavigationStack {
+                AddMovieScreen()
+            }
+        })
+        
+    }
+}
+
+#Preview {
+    NavigationStack {
+        MovieListScreen()
+            .modelContainer(for: [Movie.self])
+    }
+}
