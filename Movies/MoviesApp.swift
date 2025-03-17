a56dc68552bdcea702a63a4ad78dcb5883f683f0
misaki kawaguchi <60394359+misaki-kawaguchi@users.noreply.github.com>
2025-03-17 09:55:46 +0900

    Saving a Movie (#2)
    * Movieモデルを作成
    
    * .gitignore修正
    
    * キャッシュ削除
    
    * AddMovieScreen作成・フィームから追加できる様にする

--- Movies/MoviesApp.swift
+++ Movies/MoviesApp.swift
@@ -11,7 +11,10 @@ import SwiftUI
 struct MoviesApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationStack {
+                AddMovieScreen()
+            }
         }
+        .modelContainer(for: [Movie.self])
     }
 }
