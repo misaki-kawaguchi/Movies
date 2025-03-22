92a28f73a225c8945c8045ac406976248c294213
misaki kawaguchi <60394359+misaki-kawaguchi@users.noreply.github.com>
2025-03-22 19:32:33 +0900

    FIX: Deleting Review  (#11)
    * 削除時の不具合を修正
    
    * オプショナル削除
    
    * テキスト修正

--- Movies/Screens/AddReviewScreen.swift
+++ Movies/Screens/AddReviewScreen.swift
@@ -42,7 +42,7 @@ struct AddReviewScreen: View {
                     
                     do {
                         try context.save()
-                        movie.reviews?.append(review)
+                        movie.reviews.append(review)
                     } catch {
                         print(error.localizedDescription)
                     }
