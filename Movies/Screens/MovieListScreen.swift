9983bcb73c0528b68b62b624610cd3d16168d0f7
misaki kawaguchi <60394359+misaki-kawaguchi@users.noreply.github.com>
2025-03-23 17:35:22 +0900

    actorを保存できるようにする (#13)

--- Movies/Screens/MovieListScreen.swift
+++ Movies/Screens/MovieListScreen.swift
@@ -10,23 +10,51 @@ import SwiftData
 
 struct MovieListScreen: View {
     
+    @Environment(\.modelContext) private var context
+    
     @Query(sort: \Movie.title, order: .forward) private var movies: [Movie]
     @State private var isAddMoviePresented: Bool = false
+    @State private var isAddActorPresented: Bool = false
+    @State private var actionName: String = ""
+    
+    private func saveActor() {
+        let actor = Actor(name: actionName)
+        context.insert(actor)
+    }
     
     var body: some View {
         MovieListView(movies: movies)
-        .toolbar(content: {
-            ToolbarItem(placement: .topBarTrailing) {
-                Button("Add Movie") {
-                    isAddMoviePresented = true
+            .toolbar(content: {
+                ToolbarItem(placement: .topBarLeading) {
+                    Button("Add Actor") {
+                        isAddActorPresented = true
+                    }
                 }
-            }
-        })
-        .sheet(isPresented: $isAddMoviePresented, content: {
-            NavigationStack {
-                AddMovieScreen()
-            }
-        })
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button("Add Movie") {
+                        isAddMoviePresented = true
+                    }
+                }
+            })
+            .sheet(isPresented: $isAddActorPresented, content: {
+                Text("Add Actor")
+                    .font(.largeTitle)
+                
+                TextField("Actor name", text: $actionName)
+                    .textFieldStyle(.roundedBorder)
+                    .presentationDetents([.fraction(0.25)])
+                    .padding()
+                
+                Button("Save") {
+                    isAddActorPresented = false
+                    saveActor()
+                }
+            })
+            .sheet(isPresented: $isAddMoviePresented, content: {
+                NavigationStack {
+                    AddMovieScreen()
+                }
+            })
         
     }
 }
@@ -34,6 +62,6 @@ struct MovieListScreen: View {
 #Preview {
     NavigationStack {
         MovieListScreen()
-            .modelContainer(for: [Movie.self])
+            .modelContainer(for: [Movie.self, Review.self, Actor.self])
     }
 }
