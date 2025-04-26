14b7f4d254446ee09ad4d304a45d8f236118e6d0
misaki-kawaguchi <m.kawaguchi1122@gmail.com>
2025-04-26 16:45:50 +0900

    Movieのtitleをuniqueにする（V1からV2にマイグレーション）

--- Movies/Models/Movie.swift
+++ Movies/Models/Movie.swift
@@ -8,25 +8,4 @@
 import Foundation
 import SwiftData
 
-@Model
-final class Movie {
-    var title: String
-    var year: Int
-    var reviewsCount: Int {
-        reviews.count
-    }
-    var actorsCount: Int {
-        actors.count
-    }
-    
-    @Relationship(deleteRule: .cascade, inverse: \Review.movie)
-    var reviews: [Review] = []
-    
-    @Relationship(deleteRule: .nullify, inverse: \Actor.movies)
-    var actors: [Actor] = []
-    
-    init(title: String, year: Int) {
-        self.title = title
-        self.year = year
-    }
-}
+typealias Movie = MoviesSchemaV2.Movie
---
+++ Movies/Schema Versions/MoviesMigrationPlan.swift
@@ -0,0 +1,42 @@
+//
+//  MoviesMigrationPlan.swift
+//  Movies
+//
+//  Created by 川口美咲 on 2025/04/26.
+//
+
+import Foundation
+import SwiftData
+
+enum MoviesMigrationPlan: SchemaMigrationPlan {
+    static var schemas: [any VersionedSchema.Type] {
+        [MoviesSchemaV1.self, MoviesSchemaV2.self]
+    }
+    
+    static var stages: [MigrationStage] {
+        [migrateV1toV2]
+    }
+    
+    static let migrateV1toV2 = MigrationStage.custom(fromVersion: MoviesSchemaV1.self, toVersion: MoviesSchemaV2.self, willMigrate: {
+        context in
+        
+        guard let movies = try? context.fetch(FetchDescriptor<MoviesSchemaV1.Movie>()) else { return }
+        
+        var duplicates = Set<MoviesSchemaV1.Movie>()
+        var uniqueSet = Set<String>()
+        
+        for movie in movies {
+            if !uniqueSet.insert(movie.title).inserted {
+                duplicates.insert(movie)
+            }
+        }
+        
+        for movie in duplicates {
+            guard let movieToBeUpdated = movies.first(where: {$0.id == movie.id }) else { continue }
+            movieToBeUpdated.title = movieToBeUpdated.title + "\(UUID().uuidString)"
+        }
+        
+        try? context.save()
+        
+    }, didMigrate: nil)
+}
