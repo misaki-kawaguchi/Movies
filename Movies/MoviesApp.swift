ce7a64ce582784fd0a1409fa91387083538b5bf1
misaki kawaguchi <60394359+misaki-kawaguchi@users.noreply.github.com>
2025-04-26 17:41:42 +0900

    Movieのtitleをuniqueにする（V1からV2にマイグレーション） (#24)

--- Movies/MoviesApp.swift
+++ Movies/MoviesApp.swift
@@ -6,15 +6,27 @@
 //
 
 import SwiftUI
+import SwiftData
 
 @main
 struct MoviesApp: App {
+    
+    let container: ModelContainer
+    
+    init() {
+        do {
+            container = try ModelContainer(for: Movie.self, migrationPlan: MoviesMigrationPlan.self, configurations: ModelConfiguration(for: Movie.self))
+        } catch {
+            fatalError("Could not initialize container.")
+        }
+    }
+    
     var body: some Scene {
         WindowGroup {
             NavigationStack {
                 MovieListScreen()
             }
         }
-        .modelContainer(for: [Movie.self])
+        .modelContainer(container)
     }
 }
